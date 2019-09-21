fc9eb09cec88d4b9783952334019d06fc860bb37
danma <danma@yunify.com>
2019-09-21 09:49:36 +0800

    CoreML model use MLMultiArray input

--- onnx_to_coreml.py
+++ onnx_to_coreml.py
@@ -8,5 +8,5 @@ model_out = sys.argv[2]
 model_file = open(model_in, 'rb')
 model_proto = onnx_pb.ModelProto()
 model_proto.ParseFromString(model_file.read())
-coreml_model = convert(model_proto, image_input_names=['0'], add_custom_layers=True)
+coreml_model = convert(model_proto, add_custom_layers=True)
 coreml_model.save(model_out)
